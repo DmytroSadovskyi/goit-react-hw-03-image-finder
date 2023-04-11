07735382899f6e64f3a25aa3f91c72c4f96d2e6d
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-04-11 17:04:02 +0300

    delete

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,42 @@
+import { Component } from 'react';
+
+class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+
+  handleChange = e => {
+    const { value } = e.currentTarget;
+    this.setState({ query: value.trim() });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.query);
+    this.setState({ query: '' });
+  };
+
+  render() {
+    const { query } = this.state;
+    return (
+      <header>
+        <form onSubmit={this.handleSubmit}>
+          <button type="submit">
+            <span>Search</span>
+          </button>
+
+          <input
+            onChange={this.handleChange}
+            value={query}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+export default Searchbar;
