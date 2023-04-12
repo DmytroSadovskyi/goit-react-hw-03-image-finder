b5671080abd3977dce85dc4919a5173af0e7b9b7
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-04-12 15:48:57 +0300

    Update Modal.styled.jsx

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -6,19 +6,15 @@ export const ModalBackdrop = styled.div`
   left: 0;
   width: 100vw;
   height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: rgba(0, 0, 0, 0.5);
-  padding-bottom: 20px;
 `;
 
 export const ModalContent = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 65vw;
-  max-height: 850px;
-  background-color: #fff;
-  border-radius: 3px;
+  max-width: calc(100vw - 48px);
+  max-height: calc(100vh - 24px);
   box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
     0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
 `;
